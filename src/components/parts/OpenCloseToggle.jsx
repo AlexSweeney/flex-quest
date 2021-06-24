dd71ae1e1d9302e42fa3e483ce1e5aee4c4f697d
Alex Sweeney <alexsweeney@live.co.uk>
2021-06-24 21:53:34 +0700

    feat(OpenCloseToggle.jsx) animate open and close icon

--- src/components/parts/OpenCloseToggle.jsx
+++ src/components/parts/OpenCloseToggle.jsx
@@ -3,13 +3,9 @@ import './OpenCloseToggleStyle.css';
 
 export default function OpenCloseToggle({isOpen, handleClick}) {
 	return (
-		isOpen ? 
-			<div className="toggle" onClick={handleClick}>
-				<div className="line"></div>
-			</div> 
-		: <div className="toggle" onClick={handleClick}>
-				<div className="line"></div>
-				<div className="line vert-line"></div>
-			</div>
+		<div className="toggle-background" onClick={handleClick}>
+			<div className="line"></div>
+			<div className={isOpen ? "line" : "line vert-line"}></div>
+		</div>
 	)
 }
