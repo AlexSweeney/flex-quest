79c8291f1ab9305dc56f378e944bcb4d9ac6164f
Alex Sweeney <alexsweeney@live.co.uk>
2021-06-25 19:12:36 +0700

    feat(Burger.jsx) added click animation

--- src/components/parts/Box.jsx
+++ src/components/parts/Box.jsx
@@ -4,30 +4,37 @@ import OpenCloseToggle from './OpenCloseToggle.jsx';
 import './BoxStyle.css';
 
 export default function Box({showBurger}) {
-	const titles = [
+	/*const titles = [
 		"Level 1: display: flex;"
-	];
+	];*/
 
 	const [isExpanded, setIsExpanded] = useState(true); 
-	const [thisTitle, setThisTitle] = useState(titles[0]);
+	// const [thisTitle, setThisTitle] = useState(titles[0]);
+	const [burgerIsOpen, setBurgerIsOpen] = useState(false);
 
 	function handleOpenCloseToggleClick() {  
 		setIsExpanded(oldVal => !oldVal);
 	}
 
+	function handleBurgerClick() {
+		setBurgerIsOpen(oldVal => !oldVal);
+	}
+
 	return ( 
-	 	<div className={isExpanded ? "box expanded-box" : "box closed-box"}
-	 			onClick={handleOpenCloseToggleClick}>
+	 	<div className={(isExpanded ? "box expanded-box" : "box closed-box")}>
 	 		<div className="box-header"> 
 	 			<div className="burger-container">
-	 				<Burger/>
+	 				<Burger isOpen={burgerIsOpen} handleClick={handleBurgerClick}/>
 	 			</div>
 
 	 			<h2 className="title">Level 1: display: flex;</h2>
 	 				
 	 			<div className="toggle-container">
-	 				<OpenCloseToggle isOpen={isExpanded}/>
+	 				<OpenCloseToggle isOpen={isExpanded} handleClick={handleOpenCloseToggleClick}/>
 	 			</div>
+	 		</div>
+	 		<div className={burgerIsOpen ? "box-body burger-body" : "box-body"}>
+
 	 		</div>
 	 	</div> 
 	)
--- src/components/parts/Burger.jsx
+++ src/components/parts/Burger.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import './BurgerStyle.css';
 
-export default function Burger({open}) {
+export default function Burger({isOpen, handleClick}) {
+	const burgerClass = isOpen ? 'burger burger-open' : 'burger';
+	const burgerBarClass = isOpen ? 'burger-bar burger-bar-open' : 'burger-bar';
+
 	return (
-		<div className="burger">
-			<div className="burger-bar"></div>
-			<div className="burger-bar"></div>
-			<div className="burger-bar"></div>
+		<div className={burgerClass} onClick={handleClick}>
+			<div className={burgerBarClass}></div>
+			<div className={burgerBarClass}></div>
+			<div className={burgerBarClass}></div>
 		</div>		
 	)
 }
-
-{/*<div className={open ? "burger-container" : "burger-container burger-gone"}>
-			<div className="burger-bar"></div>
-			<div className="burger-bar"></div>
-			<div className="burger-bar"></div>
-		</div>*/}
