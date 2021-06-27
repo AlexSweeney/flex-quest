88ce5906cd0a96cdc5ee6412ba711d6d336e7131
Alex Sweeney <alexsweeney@live.co.uk>
2021-06-27 21:36:56 +0700

    feat(BurgerBar.jsx) add hover and click colors

--- src/components/parts/Burger.jsx
+++ src/components/parts/Burger.jsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './BurgerStyle.css';
 
 export default function Burger({isOpen, handleClick}) {
-	const burgerClass = isOpen ? 'burger burger-open' : 'burger';
-	const burgerBarClass = isOpen ? 'burger-bar burger-bar-open' : 'burger-bar';
+	const [isHover, setIsHover] = useState(false);
+	const [isDown, setIsDown] = useState(false);
+
+	const burgerClass = isOpen ? 'burger burger-open' : 'burger'; 
+ 	const burgerBarClass = (() => {
+ 		let thisClass = 'burger-bar';
+ 		if(isHover) thisClass += ' burger-bar-hover';
+ 		if(isDown) thisClass += ' burger-bar-down';
+ 		return thisClass;
+ 	})();
+
+	function handleMouseOver() {
+		setIsHover(true);
+	}
+
+	function handleMouseOut() {
+		setIsHover(false);
+		setIsDown(false);
+	}
+
+	function handleMouseDown() {
+		setIsDown(true);
+		handleClick();
+	}
+
+	function handleMouseUp() {
+		setIsDown(false);
+	}
 
 	return (
-		<div className={burgerClass} onClick={handleClick}>
+		<div className={burgerClass}  
+			onMouseOver={handleMouseOver}
+			onMouseOut={handleMouseOut}
+			onMouseDown={handleMouseDown}
+			onMouseUp={handleMouseUp}>
 			<div className={burgerBarClass}></div>
 			<div className={burgerBarClass}></div>
 			<div className={burgerBarClass}></div>
