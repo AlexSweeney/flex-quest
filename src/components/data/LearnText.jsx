fe2b0b99f473010481d86b55971e9c9e601d6a66
Alex Sweeney <alexsweeney@live.co.uk>
2021-06-29 16:36:47 +0700

    feat(LearnText.jsx) added data storage file

--- src/components/data/LearnText.jsx
+++ src/components/data/LearnText.jsx
@@ -1,14 +1,8 @@
 import React from 'react';
-
-import Level_1 from './LearnData/Level_1.jsx';
-
+import {learnData} from './learnData.jsx';
+ 
 export default function LearnText({levelNum}) { 
- 	const allData = [
-		Level_1,
-		/*level_2,*/
-	];
-
 	return (
-		allData[levelNum]()
+		learnData[levelNum]
 	)
 }
---
+++ src/components/data/learnData.jsx
@@ -0,0 +1,6 @@
+const level_1 = 'hello';
+
+export const learnData = [
+	level_1,
+	/*level_2,*/
+];
