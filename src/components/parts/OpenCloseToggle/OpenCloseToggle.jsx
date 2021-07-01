260fe10ac6ca241dca0d98a341c43a0b96082d4b
Alex Sweeney <alexsweeney@live.co.uk>
2021-07-02 00:50:43 +0700

    tidy(OpenCloseToggle.jsx) tidy mouse functions

--- src/components/parts/OpenCloseToggle/OpenCloseToggle.jsx
+++ src/components/parts/OpenCloseToggle/OpenCloseToggle.jsx
@@ -1,72 +1,34 @@
 import React, {useState} from 'react';
 import './OpenCloseToggleStyle.css';
 
-export default function OpenCloseToggle({isOpen, handleClick}) {
-	const [isOver, setIsOver] = useState(false);
-	const [isDown, setIsDown] = useState(false);
-
-	const horizClass = (() => {
-		let thisClass = 'line';
-		if(isOver) thisClass += ' line-over';
-		if(isDown) thisClass += ' line-down';
-		return thisClass;
-	})();
-
-	const vertClass = (() => {
-		let thisClass = 'line';
-		if(isOpen) thisClass += ' vert-line';
-		if(isOver) thisClass += ' line-over';
-		if(isDown) thisClass += ' line-down';
-		return thisClass;
-	})();
+export default function OpenCloseToggle({isOpen, handleClick}) { 
+	const [lineClass, setLineClass] = useState('');
 
 	function handleOver() {
-		setIsOver(true);
+		setLineClass('line-over');
 	}
 
 	function handleOut() {
-		setIsOver(false);
-		setIsDown(false);
+		setLineClass('');
 	}
 
 	function handleDown() {
-		setIsDown(true);
+		setLineClass('line-down');
 		handleClick();
 	}
 
 	function handleUp() {
-		setIsDown(false);
+		setLineClass('');
 	}
-
-	// const [lineClass, setLineClass] = useState('');
-
-	// function handleOver() {
-	// 	setLineClass('line-over');
-	// }
-
-	// function handleOut() {
-	// 	setLineClass('');
-	// }
-
-	// function handleDown() {
-	// 	setLineClass('line-down');
-	// 	// handleClick();
-	// }
-
-	// function handleUp() {
-	// 	setLineClass('');
-	// }
 
 	return (
 		<div className="open-close-toggle" 	
 			onMouseOver={handleOver}
 			onMouseOut={handleOut}
 			onMouseDown={handleDown}
-			onMouseUp={handleUp}>
-			<div className={horizClass}></div>
-			<div className={vertClass}></div>
-			{/*<div className={lineClass + ' line'}></div>
-			<div className={lineClass + ' vert-line'}></div>*/}
+			onMouseUp={handleUp}> 
+			<div className={lineClass + ' line'}></div>
+			<div className={lineClass + ' line' + (isOpen ? ' vert-line' : '')}></div>
 		</div> 
 	)
 }
