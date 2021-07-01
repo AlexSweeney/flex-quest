e246e51bf29724d937f77852a0d0ade03b3bae43
Alex Sweeney <alexsweeney@live.co.uk>
2021-07-01 15:46:29 +0700

    feat(OpenCloseBox.jsx) close title animation

--- src/components/parts/OpenCloseHeader.jsx
+++ src/components/parts/OpenCloseHeader.jsx
@@ -13,8 +13,8 @@ export default function OpenCloseHeader() {
 	const [isExpanded, setIsExpanded] = useState(true); 
 
 	const title = 'title example'; 
-	const menuOptions = ['a', 'b', 'c'];
-	// const burgerIsOpen = false;
+	const menuOptions = ['a', 'b', 'c'];  
+
 	function handleOpenCloseToggleClick() {  
 		if(isExpanded) closeBox();
 		else if(!isExpanded) openBox();
@@ -23,11 +23,20 @@ export default function OpenCloseHeader() {
 	function closeBox() {
 		setBurgerIsOpen(false);
 		setIsExpanded(false);
+		setTitleSize(true);
 	}
 
 	function openBox() {
 		if(burgerWasOpen) setBurgerIsOpen(true);
 		setIsExpanded(true);
+		setTitleSize(false);
+	}
+
+	function setTitleSize(open) {
+		const boxSize = document.querySelector('#box-header').getBoundingClientRect().width + 'px';
+		const width = open ? boxSize : '100%';
+		document.querySelector('#title').style.width = width;
+		console.log('boxSize', boxSize);
 	}
 
 	function handleBurgerClick() {
@@ -37,11 +46,11 @@ export default function OpenCloseHeader() {
 
 	return (
 		<div className="box">
-			<div className={isExpanded ? "box-header box-header-expanded" : "box-header box-header-minimized"}> 
+			<div className={isExpanded ? "box-header box-header-expanded" : "box-header box-header-minimized"} id="box-header"> 
 				<div className="burger-container">
 					<Burger isOpen={burgerIsOpen} isExpanding={isExpanded} menuOptions={menuOptions} handleClick={handleBurgerClick}/>
 				</div>
-				{/*<div className={ isExpanded ? "title title-opening" : "title title-closing"}>{title}</div>*/}
+				<div className={ isExpanded ? "title title-opening" : "title title-closing"} id="title">{title}</div>
 				{/*<div className="open-close-toggle-container" onClick={handleOpenCloseToggleClick}></div>*/}
 
 				<div className="open-close-toggle-container">
