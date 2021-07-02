f3e4ae1a15ebd4b6bdf07ca1ed90550b8afbf082
Alex Sweeney <alexsweeney@live.co.uk>
2021-07-02 13:37:34 +0700

    feat(OpenCloseBox) handle burger option click

--- src/components/parts/OpenCloseBox/OpenCloseBox.jsx
+++ src/components/parts/OpenCloseBox/OpenCloseBox.jsx
@@ -4,13 +4,13 @@ import Burger from '../Burger/Burger.jsx';
 import BurgerDropDown from '../Burger/BurgerDropDown.jsx';
 import './OpenCloseBox.css';
 
-export default function OpenCloseBox({menuOptions = null}) {   
+export default function OpenCloseBox({menuOptions = null, title = null}) {   
 	const [burgerIsOpen, setBurgerIsOpen] = useState(false);
 	const [burgerWasOpen, setBurgerWasOpen] = useState(false);
 
 	const [isExpanded, setIsExpanded] = useState(true); 
 
-	const [selectedTitle, setSelectedTitle] = useState('example title');  
+	const [selectedTitle, setSelectedTitle] = useState(title || menuOptions[0]);  
 
 	function handleOpenCloseToggleClick() {  
 		if(isExpanded) closeBox();
@@ -18,7 +18,7 @@ export default function OpenCloseBox({menuOptions = null}) {
 	}
 
 	function closeBox() {
-		if(burgerIsOpen) setBurgerWasOpen(true);
+		setBurgerWasOpen(burgerIsOpen);
 		setBurgerIsOpen(false);
 		setIsExpanded(false); 
 	}
@@ -30,6 +30,11 @@ export default function OpenCloseBox({menuOptions = null}) {
 		}
 		setIsExpanded(true); 
 	} 
+
+	function handleBurgerTitleClick(option) {
+		setSelectedTitle(option);
+		setBurgerIsOpen(false);
+	}
 
 	return ( 
 		<div className={isExpanded ? "box box-expanded" : "box box-minimized"}> 
@@ -47,7 +52,7 @@ export default function OpenCloseBox({menuOptions = null}) {
 	 			</div>
 			</div>
 			<div className="box-body"> 
-				{menuOptions && <BurgerDropDown isOpen={burgerIsOpen} options={menuOptions} handleOptionClick={() => {console.log('hello')}}/>}
+				{menuOptions && <BurgerDropDown isOpen={burgerIsOpen} options={menuOptions} handleOptionClick={handleBurgerTitleClick}/>}
 				<p className={ isExpanded ? "text-body" : "text-body no-show"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 			</div>
 	 	</div> 
