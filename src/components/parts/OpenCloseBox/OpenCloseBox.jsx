7aeeef51694fcd9167d61acda41e602b1fc17ded
Alex Sweeney <alexsweeney@live.co.uk>
2021-07-02 12:06:02 +0700

    feat(OpenCloseBox.jsx) body text

--- src/components/parts/OpenCloseBox/OpenCloseBox.jsx
+++ src/components/parts/OpenCloseBox/OpenCloseBox.jsx
@@ -1,6 +1,3 @@
-// auto find width for title when close
-// when open show burger first
-
 import React, {useState} from 'react';
 import OpenCloseToggle from '../OpenCloseToggle/OpenCloseToggle.jsx'; 
 import Burger from '../Burger/Burger.jsx';
@@ -51,6 +48,7 @@ export default function OpenCloseBox({menuOptions = null}) {
 			</div>
 			<div className="box-body"> 
 				{menuOptions && <BurgerDropDown isOpen={burgerIsOpen} options={menuOptions} handleOptionClick={() => {console.log('hello')}}/>}
+				<p className={ isExpanded ? "text-body" : "text-body no-show"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 			</div>
 	 	</div> 
 	);
