bedc5ec6b69e63cbb78c56377c77a5dd75263141
Alex Sweeney <alexsweeney@live.co.uk>
2021-08-31 06:00:21 +0700

    tidy(DisplayBox.jsx) delete old

--- src/components/DisplayBox.jsx
+++ src/components/DisplayBox.jsx
@@ -7,13 +7,6 @@ import './DisplayBox.css';
 
 export default function DisplayBox({title, htmlString, cssString, fade}) {    
   const [source, setSource] = useState(null);
-  // const iframeStyle = '* {padding: 0; margin: 0; transition: opacity 2s; overflow: hidden;} *:hover { cursor: default; }';
-
-  // const fadeStyle = fade ? { opacity: 0 } : {};
-  const initialIframeStyle = {
-  	width: '100%',
-  	height: '100%',
-  }
 
   const handleRefresh = function() {
 		const iframeElement = document.getElementById('iframe');
@@ -27,24 +20,14 @@ export default function DisplayBox({title, htmlString, cssString, fade}) {
 			iframeElement.style.height = '100%'; 
 		}, 1000); 
 	}
-  
-  // useEffect(() => { 
-  // 	// setSource(htmlString + `<style>${cssString} ${iframeStyle} ${fadeStyle}</style>`);
-  // }, [htmlString, cssString])
 
   useEffect(() => { 
   	setSource(htmlString + `<style>body { padding: 0; margin: 0; } ${cssString}</style>`);
   }, [htmlString, cssString]) 
-  
-  /*useEffect(() => {
-  	const iframeElement = document.getElementById('iframe');
-  	iframeElement.style.padding = 0;
-  }, [])*/
-  
+   
 	return (
 		<OpenCloseBox title={title} handleRefresh={handleRefresh} bodyClass="iframe-background"> 
-			<iframe srcdoc={source} className="iframe" id="iframe"/>   
-			{/*<iframe srcdoc={source} style={defaultIframeStyle} className="iframe" id="iframe"/>*/}
+			<iframe srcdoc={source} className="iframe" id="iframe"/>    
 		</OpenCloseBox>
 	) 
 }
