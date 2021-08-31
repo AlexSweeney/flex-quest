967e4074590e1d2819a7d03503b22baa25cb9fc0
Alex Sweeney <alexsweeney@live.co.uk>
2021-08-31 09:37:32 +0700

    feat(DisplayBox.jsx) grid adjusts to iframe size

--- src/components/DisplayBox.jsx
+++ src/components/DisplayBox.jsx
@@ -1,5 +1,4 @@
-// animate refresh
-// handle input for iframe
+// add grid toggle
 
 import React, {useState, useEffect, useRef} from 'react'; 
 import OpenCloseBox from './parts/OpenCloseBox/OpenCloseBox.jsx';  
@@ -21,13 +20,74 @@ export default function DisplayBox({title, htmlString, cssString, fade}) {
 		}, 1000); 
 	}
 
-  useEffect(() => { 
-  	setSource(htmlString + `<style>body { padding: 0; margin: 0; } ${cssString}</style>`);
-  }, [htmlString, cssString]) 
+	const [showGrid, setShowGrid] = useState(true);
+
+	const grid = `
+	<div class="grid-container"> 
+		<div class="grid-line-vert"></div>
+		<div class="grid-line-vert"></div>
+		<div class="grid-line-vert"></div>
+		<div class="grid-line-vert"></div>
+		<div class="grid-line-vert"></div>
+		<div class="grid-line-vert"></div>
+		<div class="grid-line-vert"></div>
+		<div class="grid-line-vert"></div>
+		<div class="grid-line-vert"></div>
+
+		<div class="grid-line-horiz" style="left: 0"></div>
+		<div class="grid-line-horiz" style="left: 10%"></div>
+		<div class="grid-line-horiz" style="left: 20%"></div>
+		<div class="grid-line-horiz" style="left: 30%"></div>
+		<div class="grid-line-horiz" style="left: 40%"></div>
+		<div class="grid-line-horiz" style="left: 50%"></div>
+		<div class="grid-line-horiz" style="left: 60%"></div>
+		<div class="grid-line-horiz" style="left: 70%"></div>
+		<div class="grid-line-horiz" style="left: 80%"></div> 
+	</div>`;
+
+	const defaultStyles = `
+		body { 
+			padding: 0; 
+			margin: 0; 
+		} 
+
+		* {
+			box-sizing: border-box;
+		}
+	`;
+
+	const gridStyle = `
+		.grid-container {
+			width: 100%;
+			height: 100%;
+			position: absolute;
+			border: 2px solid #2196f3;
+		}
+
+		.grid-line-vert {
+			border-bottom: 2px solid #2196f3;
+			height: 10%;
+			width: 100%;
+		}
+
+		.grid-line-horiz {
+			border-right: 2px solid #2196f3;
+			height: 100%;
+			width: 10%;
+			position: absolute;
+		  top: 0;
+		}`;
+
+  useEffect(() => {  
+  	const newString = grid;
+  	const newStyle = defaultStyles + gridStyle; 
+
+  	setSource(`<style>${newStyle}</style> ${newString}`);
+  }, [htmlString, cssString, showGrid]) 
    
 	return (
-		<OpenCloseBox title={title} handleRefresh={handleRefresh} bodyClass="iframe-background"> 
-			<iframe srcdoc={source} className="iframe" id="iframe"/>    
+		<OpenCloseBox title={title} handleRefresh={handleRefresh} bodyClass="display-box-background">
+			<iframe srcdoc={source} className="iframe"id="iframe"/> 
 		</OpenCloseBox>
 	) 
 }
