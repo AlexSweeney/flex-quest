3bf870d4d56ad8eb76f887b0e9f756a342daf022
Alex Sweeney <alexsweeney@live.co.uk>
2021-09-01 21:41:23 +0700

    feat(Header.css) selected color for header links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import './Header.css';
 
-export default function Header() {
+export default function Header() { 
+	const [activeLink, setActiveLink] = useState(window.location.pathname);
+	console.log('activeLink', activeLink);
+
+	/*function handleLinkClick(pathname) {
+		setActiveLink(pathname);
+	}*/
+
+	function handleLinkClick(pathname) {
+		setActiveLink(pathname);
+	}
+
 	return (
 		<header class="header">
 			<Link to="/">
-		  	<h1>Flex Quest</h1>
+		  	<h1 className={activeLink === '/' ? 'link-selected' : 'link'} 
+		  			onClick={() => handleLinkClick('/')}>Flex Quest</h1>
 		  </Link>
 		  <nav>
 		  	<ul class="header-options">
 		  		{/*<li>Intro</li>*/}
 		  		<Link to="/learn">
-		  			<li>Learn</li>
+		  			<li className={activeLink === '/learn' ? 'link-selected' : 'link'} 
+		  					onClick={() => handleLinkClick('/learn')}>Learn</li>
 		  		</Link>
 		  		<Link to="/play">
-		  			<li>Play</li>
+		  			<li className={activeLink === '/play' ? 'link-selected' : 'link'} 
+		  					onClick={() => handleLinkClick('/play')}>Play</li>
 		  		</Link>
 		  		{/*<li>Read</li>
 		  		<li>Write</li>
