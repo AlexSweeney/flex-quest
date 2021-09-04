08a5644311a67b1dfec8ecefd48d41986564523e
Alex Sweeney <alexsweeney@live.co.uk>
2021-09-04 07:14:16 +0700

    fix(RefreshButton.jsx) keep color until animation finished when click multiple times

--- src/components/parts/RefreshButton.jsx
+++ src/components/parts/RefreshButton.jsx
@@ -1,22 +1,83 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import RefreshIcon from '@material-ui/icons/Refresh';
 import './RefreshButton.css';
 
 export default function RefreshButton({onClick}) {
 	const [rotateNum, setRotateNum] = useState(0);
-	
-	function refreshDown() {
-		onClick();
-		setRotateNum(oldVal => oldVal + 360);
-	}
+	const [refreshClass, setRefreshClass] = useState('refresh-button');
+	const [isDown, setIsDown] = useState(false);
+	const [isOver, setIsOver] = useState(false);
+	const [isAnimating, setIsAnimating] = useState(false); 
+	const [oldTimer, setOldTimer] = useState(null);
 
 	const iconStyle = {
 		'transform': `rotate(${rotateNum}deg)`,
 		'transition': 'transform 1000ms',
 	}
 
+	function handleMouseOver() {
+		setIsOver(true);
+	}
+
+	function handleMouseOut() {
+		setIsOver(false);
+		setIsDown(false);
+	}
+
+	function handleMouseDown() {
+		onClick();
+		setIsDown(true);
+		setRotateNum(oldVal => oldVal + 360);
+
+		setIsAnimating(true);
+		turnAnimationOff();
+		
+	}
+
+	function handleMouseUp() {
+		setIsDown(false);
+	}
+
+	function turnAnimationOff() {
+		clearTimeout(oldTimer);
+
+		const timer = setTimeout(() => {
+      setIsAnimating(false);
+		}, 1000);
+
+		setOldTimer(timer);
+	}
+
+	useEffect(() => { 
+		let newClass = 'refresh-button';
+
+		if(isDown || isAnimating && !isOver) {
+			newClass += ' refresh-button-down';
+		} else if(isOver && !isDown && !isAnimating) {
+			newClass += ' refresh-button-over';
+		}
+
+		setRefreshClass(newClass);
+	}, [isDown, isOver, isAnimating])
+
+	/* useEffect(() => {
+	 	console.log('timer start');
+
+    const timer = setTimeout(() => {
+    	console.log('timer finished');
+      setIsAnimating(false);
+    }, 1000);
+
+    console.log('timer', timer);
+    return () => clearTimeout(timer);
+  }, [isAnimating]);*/
+
 	return ( 
-		<div className="refresh-button" onMouseDown={refreshDown}>
+		<div className={refreshClass}
+			onMouseOver={handleMouseOver}
+			onMouseOut={handleMouseOut}
+			onMouseDown={handleMouseDown}
+			onMouseUp={handleMouseUp}>
 			<RefreshIcon className="refresh-icon" style={iconStyle} fontSize="inherit"/>
 		</div>
 	)
