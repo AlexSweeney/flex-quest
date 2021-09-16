5fa0e9731db43c503eb26020f6ce400975c786b1
Alex Sweeney <alexsweeney@live.co.uk>
2021-09-16 10:31:36 +0700

    refactor(GridButton.jsx) use state so can share with GridOverlay

--- src/components/parts/GridButton.jsx
+++ src/components/parts/GridButton.jsx
@@ -1,11 +1,80 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import GridOnIcon from '@material-ui/icons/GridOn';
 import './GridButton.css';
 
-export default function GridButton({handleClick, showGrid}) {
+export default function GridButton({handleClick}) {
+	const [gridStatus, setGridStatus] = useState('');
+	// ====================  Status ========================= //
+	const [isOver, setIsOver] = useState(false);
+	const [isDown, setIsDown] = useState(false);
+	const [isSelected, setIsSelected] = useState(false);
+
+	// ====================  Class ========================= //
+	const [gridStatusClass, setGridStatusClass] = useState('');
+
+	// ====================  Event Handlers ========================= //
+	function handleMouseOver() {
+		setIsOver(true)
+	}
+
+	function handleMouseOut() {
+		setIsOver(false)
+		setIsDown(false)
+	}
+
+	function handleMouseDown() {
+		setIsDown(true)
+		setIsSelected(oldVal => !oldVal)
+	}
+
+	function handleMouseUp() {
+		setIsDown(false)
+	}
+
+	// ====================  Update Status ========================= //
+	useEffect(() => {
+		if(isOver) {
+			if(isDown) setGridStatus('grid-down')
+			else setGridStatus('grid-over')
+		}
+
+		if(!isOver) {
+			if(isSelected) setGridStatus('grid-selected')
+			else setGridStatus('grid-out')
+		}
+
+	}, [isOver, isDown, isSelected])
+
+	// ====================  Update Class ========================= //
+	useEffect(() => {
+		let newClass;
+
+		if(gridStatus === 'grid-over') newClass = 'grid-button-over';
+		if(gridStatus === 'grid-out') newClass = 'grid-button-out';
+		if(gridStatus === 'grid-down') newClass = 'grid-button-down';
+		if(gridStatus === 'grid-selected') newClass = 'grid-button-selected';
+
+		setGridStatusClass(newClass)
+	}, [gridStatus])
+
+	// ====================  Console.logs ========================= //
+	useEffect(() => {
+		console.log('gridStatus', gridStatus)
+	}, [gridStatus])
+
+	// ====================  Output ========================= //
 	return (
-		<div onMouseDown={handleClick} className={showGrid ? "grid-button grid-button-selected" : 'grid-button'}>
+		<div onMouseOver={handleMouseOver}
+			onMouseOut={handleMouseOut}
+			onMouseDown={handleMouseDown} 
+			onMouseUp={handleMouseUp}
+			className={`grid-button ${gridStatusClass}`}>
 			<GridOnIcon className="grid-icon" fontSize="inherit"/>
 		</div>
 	)
 }
+/*
+<div onMouseDown={handleClick} 
+			className={showGrid ? "grid-button grid-button-selected" : "grid-button"}>
+			<GridOnIcon className="grid-icon" fontSize="inherit"/>
+		</div>*/
