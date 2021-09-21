1520a41eac6eef7681399f2d24306bd37392d277
Alex Sweeney <alexsweeney@live.co.uk>
2021-09-21 13:09:55 +0700

    feat(CodeInput.jsx) update code on change

--- src/components/parts/CodeInput.jsx
+++ src/components/parts/CodeInput.jsx
@@ -2,22 +2,26 @@ import React, {useState, useEffect} from 'react';
 import OpenCloseBox from './OpenCloseBox/OpenCloseBox.jsx';
 import './CodeInput.css';
 
-export default function CodeInput({title, code}) {
+export default function CodeInput({title, code, setCode, originalCode}) {
 	/*
 		* show title
 
-		show code
+		* show code
 
-		update code on user input
+		* update code on user input
 	
 		reset to original code on refresh press
 
 		open and close on toggle press 
 	*/
 
+	function handleChange(e) { 
+		setCode(e.target.value)
+	}
+
 	return (
 		<OpenCloseBox title={title}>
-			<textarea className="code-display" value={code}> 
+			<textarea className="code-display" value={code} onChange={handleChange}> 
 			</textarea>
 		</OpenCloseBox>
 	)
