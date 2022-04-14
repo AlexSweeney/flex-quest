5c010699db528b149494397d66811ed603d774cc
Alex Sweeney <a.sweeney@ryte.com>
2022-04-14 20:13:55 +0100

    Header.js - refactor logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './components/Header.jsx';
+import Header from './components/Header/Header.jsx';
 import Home from './pages/Home/Home.jsx';
 import Learn from './components/Learn.jsx';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,79 @@
+import React, {useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
+import './Header.scss';
+
+export default function Header() { 
+	const [activeLink, setActiveLink] = useState(window.location.pathname); 
+	const [logoClass, setLogoClass] = useState('header-logo');
+	const [learnClass, setLearnClass] = useState('header-link');
+
+	// ================== Util Fns
+	const updateLogoClass = (activeLink) => {
+		let newClass;
+		
+		if(['/', '/home', '/flex-quest'].indexOf(activeLink) !== -1) {
+			newClass = 'header-logo header-logo__selected';
+		} else {
+			newClass = 'header-logo';
+		}
+
+		setLogoClass(newClass)
+	}
+
+	const updateLearnClass = (activeLink) => {
+		let newClass;
+
+		if(activeLink === '/learn') {
+			newClass = 'header-link header-link__selected';
+		} else {
+			newClass = 'header-link';
+		}
+
+		setLearnClass(newClass);
+	};
+
+	// ================== Event Handlers
+	const onChangeActiveLink = (activeLink) => {
+		updateLogoClass(activeLink);
+		updateLearnClass(activeLink); 
+	}
+ 
+	const onLinkClick = (pathname) => {
+		setActiveLink(pathname);
+	};
+
+	// ================== Listeners
+	useEffect(() => {
+		onChangeActiveLink(activeLink)
+	}, [activeLink])
+
+	// ================== Output
+	return (
+		<header className="header">
+			<Link to="/">
+				<h1 className={logoClass}  
+		  			onMouseDown={() => onLinkClick('/')}>Flex Quest</h1>
+		  	{/* <h1 className={activeLink === '/' ? 'link link-selected' : 'link'}  
+		  			onMouseDown={() => handleLinkClick('/')}>Flex Quest</h1> */}
+		  </Link>
+		  <nav>
+		  	<ul className="header-options">
+		  		{/*<li>Intro</li>*/}
+		  		<Link to="/learn">
+		  			<li className={learnClass} 
+		  					onClick={() => onLinkClick('/learn')}>Learn</li>
+		  		</Link>
+		  		{/*<Link to="/play">
+		  			<li className={activeLink === '/play' ? 'link link-selected' : 'link'} 
+		  					onClick={() => handleLinkClick('/play')}>Play</li>
+		  		</Link>*/}
+		  		{/*<li>Read</li>
+		  		<li>Write</li>
+		  		<li>Use</li>
+		  		<li>Reference</li>
+		  		<li>Support</li>*/}
+		  	</ul>
+		  </nav>
+		</header>
+	)
+}
